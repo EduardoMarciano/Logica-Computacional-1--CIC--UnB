90bb393d2a5b46f64375fb5b55ffc9ae5a16a612
Eduardo Marciano <e.marciano.meneses@gmail.com>
2023-04-30 14:20:06 -0300

    update

---
+++ Cap 1.3 -  A Lógica Proposicional Intuicionista/Exercícios.v
@@ -0,0 +1,49 @@
+Section Cap3.
+Variables p u: Prop.
+Hypothesis H : (~~p) -> (~~u).
+
+Lemma Exe1 :  ~~(p->u).
+Proof.
+  intro.
+  apply H0.
+  intro.
+  exfalso.
+  apply H.
+  intro.
+  contradiction.
+  intro.
+  apply H0.
+  intro.
+  assumption.
+
+Qed.
+End Cap3.
+
+Section Cap3.
+Variables p u: Prop.
+Lemma Exe2 :  ~~(~~p->p).
+Proof.
+  intro.
+  apply H.
+  intro.
+  exfalso.
+  apply H0.
+  intro.
+  apply H.
+  intro.
+  assumption.
+Qed.
+
+Lemma Exe3 :  ~~(((p -> u) -> p) -> p).
+Proof.
+  intro.
+  apply H.
+  intro.
+  apply H0.
+  intro.
+  exfalso.
+  apply H.
+  intro.
+  assumption.
+Qed.
+End Cap3.
--- Exemplos.v
+++ Exemplos.v
@@ -4,30 +4,10 @@ Hypothesis H : p.
 Lemma Exe1 :  ~~p.
 Proof.
     intro.
-    apply H0.
-    assumption.
-Qed.
-
-
-End Exemplos.
-
-Section Exemplos.
-Variables p u: Prop.
-Hypothesis H : (~~p) -> (~~u).
-
-Lemma Exe2 :  ~~(p->u).
-Proof.
-  intro.
-  apply H0.
-  intro.
-  exfalso.
-  apply H.
-  intro.
-  contradiction.
-  intro.
-  apply H0.
-  intro.
-  assumption.
-
+    contradiction.
 Qed.
 End Exemplos.
+
+
+
+
