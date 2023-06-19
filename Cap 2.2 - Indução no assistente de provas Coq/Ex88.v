9f52f15140132a6cef70a4c43a4c254810a99686
Eduardo Marciano <e.marciano.meneses@gmail.com>
2023-06-19 00:01:10 -0300

    Up

--- Cap 2.2 - Indução no assistente de provas Coq/Ex88.v
+++ Cap 2.2 - Indução no assistente de provas Coq/Ex88.v
@@ -2,14 +2,14 @@ Lemma PIG: forall (P : nat -> Prop) (k : nat), P k ->
 (forall n, n >= k -> P n -> P (S n)) ->
 forall n : nat, n >= k -> P n.
 Proof.
-  intros P k IH n H2.
+  intros P k H1 IH n H2.
   assert (H := nat_ind (fun n => n >= k -> P n)).
   generalize dependent n. apply H.
   - intro Hk. inversion Hk. subst.
   apply H1.
-  - intros n H2 H3 in H1. assumption.
-  + rewrite H0 in H1. assumption.
-  + subst. apply IH.
-    * assumtion.
-    * apply H2. assumption.
+  - intros n H2 H3. inversion H3.
+    + rewrite H0 in H1. assumption.
+    + subst. apply IH.
+      * assumption.
+      * apply H2. assumption.
 Qed.
